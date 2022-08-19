dfeb37a4092f368da55e71893f73b751d09ab48c
Flavio Fernandes Junior <100242594+flaviofernandesjunior@users.noreply.github.com>
2022-08-19 00:54:02 -0300

    teste da aula de js intermediário

---
+++ script.js
@@ -0,0 +1,8 @@
+let usuario = "fernando";
+let usuarioatual = null;
+if (usuario === "flavio") {
+   usuarioatual = usuario;
+} else {
+    console.log("nao eh o fernando")
+}
+console.log(usuarioatual)
